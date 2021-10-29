9ab80b187ec9ea8c2ff892ada24aa4867462381f
datagutten <datagutten@datagutten.net>
2021-10-29 21:14:20 +0200

    Add list view for events and ups in admin

--- upsinfo/admin.py
+++ upsinfo/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Ups
+from .models import Ups, Event
 
-admin.site.register(Ups)
+
+@admin.register(Ups)
+class UpsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'ip', 'vendor']
+    list_filter = ['vendor']
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['ups', 'time', 'event']
+    list_filter = ['ups']
