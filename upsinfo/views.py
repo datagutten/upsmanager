bbaf0bf5cdd45b09e99acbf1829effef77a4d275
datagutten <datagutten@datagutten.net>
2022-02-18 15:06:43 +0100

    Handle HTTP error

--- upsinfo/views.py
+++ upsinfo/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from requests import HTTPError
+
 from .models import Ups
 
 
@@ -13,6 +15,8 @@ def ups_list(request):
             info_list.append({'snmp': snmp_obj, 'ups': ups})
         except ValueError as e:
             # snmp.append({'name': ups.name, 'status': 'Offline'})
+            info_list.append({'ups': ups, 'error': e})
+        except HTTPError as e:
             info_list.append({'ups': ups, 'error': e})
 
     return render(request, 'upsinfo/ups_list.html', {'ups_list': info_list})
