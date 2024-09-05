8e9f154f5e4ccc2c5ed6530534ae9f334d671c5b
datagutten <datagutten@datagutten.net>
2024-09-05 21:14:49 +0200

    Add prometheus exporter

--- upsinfo/urls.py
+++ upsinfo/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import web
+from .views import web, metrics
 
 app_name = 'upsinfo'
 urlpatterns = [
     path('', web.ups_list, name='index'),
     path('events', web.events, name='events'),
+    path('metrics', metrics.metrics, name='metrics'),
 ]
---
+++ upsinfo/views/metrics.py
@@ -0,0 +1,34 @@
+import prometheus_client
+from django.http import HttpResponse
+
+from upsinfo import models
+
+labels = {'ups_name': 'UPS Name', 'ups_ip': 'UPS IP'}
+load = prometheus_client.Gauge('ups_load', 'UPS load percentage', labels)
+battery_temperature = prometheus_client.Gauge('ups_battery_temperature', 'Battery temperature in centigrade',
+                                              labels)
+battery = prometheus_client.Gauge('ups_battery', 'UPS battery percentage', labels)
+battery_voltage = prometheus_client.Gauge('ups_battery_voltage', 'Battery voltage', labels)
+input_voltage = prometheus_client.Gauge('ups_input_voltage', 'UPS input voltage', labels)
+runtime = prometheus_client.Gauge('ups_runtime', 'UPS runtime in seconds', labels)
+status_string = prometheus_client.Info('ups_status_string', 'UPS status string', labels)
+
+
+def metrics():
+    for ups_obj in models.Ups.objects.filter(enabled=True):
+        ups_labels = [ups_obj.name, ups_obj.ip]
+        try:
+            ups_snmp = ups_obj.snmp()
+
+            load.labels(*ups_labels).set(ups_snmp.load())
+            battery_temperature.labels(*ups_labels).set(ups_snmp.battery_temperature())
+            battery.labels(*ups_labels).set(ups_snmp.battery())
+            battery_voltage.labels(*ups_labels).set(ups_snmp.battery_voltage())
+            input_voltage.labels(*ups_labels).set(ups_snmp.input_voltage())
+            runtime.labels(*ups_labels).set(ups_snmp.runtime_seconds())
+            status_string.labels(*ups_labels).info({'status': ups_snmp.status_string()})
+        except Exception as e:
+            status_string.labels(*ups_labels).info({'status': str(e)})
+            continue
+    output = prometheus_client.generate_latest()
+    return HttpResponse(output, content_type='text/plain', charset='utf-8')
