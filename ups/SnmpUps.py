b96df6861e1f6b37736116cb7550bb2b87797dd7
datagutten <datagutten@datagutten.net>
2024-12-02 21:09:55 +0100

    Do not set SNMP version

--- ups/SnmpUps.py
+++ ups/SnmpUps.py
@@ -7,9 +7,9 @@ from ups import BaseUPS, exceptions
 
 
 class SnmpUps(BaseUPS, ABC):
-    def __init__(self, ip, community='public', version=0):
+    def __init__(self, ip, community='public'):
         snmp = snmp_compat.compat.select('ezsnmp')
-        self.session = snmp(ip, community, version=version, timeout=0.1)
+        self.session = snmp(ip, community, timeout=0.1)
 
     def close(self):
         self.session.close()
