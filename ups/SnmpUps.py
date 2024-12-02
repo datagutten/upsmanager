073e222cd0e3f42dd372bc3c679098dd4d877d21
datagutten <datagutten@datagutten.net>
2024-12-02 19:03:37 +0100

    Add SNMP-helper compat module with ezsnmp

--- ups/SnmpUps.py
+++ ups/SnmpUps.py
@@ -1,40 +1,24 @@
-import datetime
-import re
 from abc import ABC
 
-import netsnmp
+import snmp_compat.compat
+from snmp_compat import snmp_exceptions
 
 from ups import BaseUPS, exceptions
 
 
 class SnmpUps(BaseUPS, ABC):
-    session: netsnmp.SNMPSession
-
     def __init__(self, ip, community='public', version=0):
-        self.session = netsnmp.SNMPSession(ip, community, version=version, timeout=0.1)
+        snmp = snmp_compat.compat.select('ezsnmp')
+        self.session = snmp(ip, community, version=version, timeout=0.1)
+
+    def close(self):
+        self.session.close()
 
     def get(self, oid):
-        # noinspection PyProtectedMember
         try:
-            data = self.session.get(oid)
-        except netsnmp._api.SNMPError as e:
-            if str(e).strip() == 'Timeout':
-                raise exceptions.UPSTimeout(e)
-            else:
-                raise exceptions.UPSError(e)
-        except Exception as e:
+            response = self.session.get(oid)
+            return response.typed_value()
+        except snmp_exceptions.SNMPTimeout as e:
+            raise exceptions.UPSTimeout(e)
+        except snmp_exceptions.SNMPError as e:
             raise exceptions.UPSError(e)
-
-        data_type = data[0][1]
-        value = data[0][2]
-        if data_type == 'STRING':
-            return value[1:-1]
-        elif data_type == 'Gauge32':
-            return int(value)
-        elif data_type == 'INTEGER':
-            return int(value)
-        elif data_type == 'Timeticks':
-            matches = re.match(r'([0-9]+):([0-9]+):([0-9]+):([0-9]+)\.([0-9]+)', value)
-            return datetime.timedelta(hours=int(matches[2]), minutes=int(matches[3]),
-                                      seconds=int(matches[4]))
-        return None
