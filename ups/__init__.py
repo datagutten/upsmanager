e1da9c621020a45a82b8ecb9ab89a2d388686a8e
datagutten <datagutten@datagutten.net>
2024-12-02 19:32:04 +0100

    Remove UPS-specific classes from module init

--- ups/__init__.py
+++ ups/__init__.py
@@ -1,11 +1,2 @@
 from .BaseUPS import BaseUPS
-from .SnmpUps import SnmpUps
-from .rfc import RfcUps
-
-from .APCSmartConnectUPS import APCSmartConnectUPS
-from .Eaton import Eaton
-from .apc import ApcUps
-from .EPPC import PowerWalkerEPPC
-from .NextUPSSystems import NextUPSSystems
-
 from .selectUPS import select
