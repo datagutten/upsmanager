5c7e6eff3f131d436a5150347a3ac1a16996dcde
Anders Birkenes <datagutten@datagutten.net>
2025-11-26 17:36:54 +0100

    Correct oid for Eaton battery temperature

--- ups/Eaton.py
+++ ups/Eaton.py
@@ -6,4 +6,4 @@ class Eaton(RfcUps):
         return self.get('.1.3.6.1.4.1.534.1.2.4.0')
 
     def battery_temperature(self):
-        return self.get('.1.3.6.1.4.1.534.1.6.1')
+        return self.get('.1.3.6.1.4.1.534.1.6.1.0')
